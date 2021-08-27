82a3b339c0cb674a0eb0a9f926ccb80ac9ec4ffb
unknown <mobinh264@gmail.com>
2021-08-28 02:08:21 +0600

    with out error handling code almost done

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,24 +1,27 @@
 const searchButton = () => {
    const searchField =  document.getElementById('input-field');
    const searchText = searchField.value;
-   searchField.value = '';
    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
    fetch(url)
    .then( res => res.json())
    .then( result => displaySearchResult(result.meals))
 }
 
+// searchButton()
 
 const displaySearchResult = meals => {
+
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
     
     const searchContainer = document.getElementById('search-container');
 
     meals.forEach( meal => {
-         console.log(meal)
         const div =  document.createElement('div');
+       
         div.classList.add('col');
         div.innerHTML =`
-        <div class="card h-100">
+        <div class="card h-100" style="cursor: pointer;" onclick="loadMoreDetails('${meal.idMeal}')">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -28,4 +31,36 @@ const displaySearchResult = meals => {
         `;
         searchContainer.appendChild(div);
     })
+};
+
+
+const loadMoreDetails = id => {
+    
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+    fetch(url)
+    .then( res => res.json())
+    .then( data => displayMoreDetails(data.meals[0]))    
+}
+
+const displayMoreDetails = ( data ) =>{
+// console.log()
+    // const imgUrl = data
+    const detailsContainer = document.getElementById('details-container');
+    const div = document.createElement('div');
+    div.textContent = '';
+    div.innerHTML = `
+        <div class="col-md-6 mx-auto shadow-lg p-3 mb-5 rounded">
+            <div class="card text-white">
+                <img src="${data.strMealThumb}" class="card-img" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title fw-bold">${data.strMeal}</h5>
+                    <p class="card-text fw-bold">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <a href="${data.strYoutube}" class="href="#" style="text-decoration: none; font-weight: 700;" target="_blank" >If You want see Our Channel</a>
+                </div>
+            </div>
+        </div>
+    `;
+
+    detailsContainer.appendChild(div)
+
 }
